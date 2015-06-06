54b58d9b5328a3ffa4517741da760626e024a3a8
renjunqu <renjunqu@gmail.com>
2015-06-07 03:50:33 +0800

    2015.06.07.1

--- src/main/java/com/joymove/test/DoPost.java
+++ src/main/java/com/joymove/test/DoPost.java
@@ -1,11 +1,15 @@
 package com.joymove.test;
 
+import com.joymove.entity.JOYNCar;
 import org.json.simple.JSONObject;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component("DoPost")
 public class DoPost {
@@ -15,14 +19,25 @@ public class DoPost {
 		throw new Exception("heelo world");
 	}
 	public static void main(String [] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:**/applicationContext-mvc.xml");
 		try {
-			DoPost dopost = (DoPost) context.getBean("DoPost");
-			dopost.doPost();
-		} catch(Exception e){
-			System.out.println("show stack trace ===>>>>>>>>>>>>>>>>>>>");
+			JOYNCar car = new JOYNCar();
+			Map<String, Object> test = new HashMap<String, Object>();
+			test.put("vinNum", "hahah");
+			test.put("licensenum", "123456");
+			Field[] nCar_fields = car.getClass().getFields();
+			for (Field ncar_f : nCar_fields) {
+				if (test.get(ncar_f.getName())!=null && String.valueOf(test.get(ncar_f.getName())).length() > 0) {
+					ncar_f.setAccessible(true);
+					ncar_f.set(car, test.get(ncar_f.getName()));
+				}
+			}
+			for (Field ncar_f : nCar_fields) {
+			     System.out.println("name: "+ncar_f.getName()+" value: "+ncar_f.get(car));
+			}
+			System.out.println(car);
+		} catch (Exception e){
 			e.printStackTrace();
-			System.out.println("show stack trace over <<<<<<<<<<<<<<<<<<<<<<<===>>>>>>>>>>>>>>>>>>>");
 		}
+		return ;
 	}
 }
