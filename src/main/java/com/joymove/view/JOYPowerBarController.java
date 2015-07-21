51c7e1aad07ec5579f5c69243d61eda134ac37a2
renjunqu <renjunqu@gmail.com>
2015-07-22 00:48:44 +0800

    postgres 版本的超级快速的充电桩获取OK了。

--- src/main/java/com/joymove/view/JOYPowerBarController.java
+++ src/main/java/com/joymove/view/JOYPowerBarController.java
@@ -1,10 +1,15 @@
 package com.joymove.view;
+import java.sql.Types;
 import java.util.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.futuremove.cacheServer.utils.CoordinatesUtil;
+import com.futuremove.cacheServer.utils.Gps;
+import com.joymove.entity.*;
+import com.joymove.postgres.entity.JOYPowerBar;
 import org.apache.commons.collections.iterators.EntrySetMapIterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -21,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.joymove.entity.*;
+import com.joymove.postgres.entity.*;
 import com.joymove.service.JOYPowerBarService;
 
 
@@ -57,7 +62,7 @@ public class JOYPowerBarController extends  JOYBaseController {
 
 
 
-
+/*
 	@RequestMapping(value="rent/getNearByPowerBars", method=RequestMethod.POST)
 	public  @ResponseBody JSONObject getNearByPowerBars(HttpServletRequest req){
 		 logger.trace("getNearByPowerBars method was invoked...");
@@ -91,6 +96,64 @@ public class JOYPowerBarController extends  JOYBaseController {
 			 logger.error(e.getStackTrace().toString());
 		 }
 		 return Reobj;
+	}
+	*/
+    @Transactional(propagation= Propagation.REQUIRED,rollbackFor = Exception.class) // 增加这个事务是为了防止两次读取不一致
+	@RequestMapping(value="rent/getNearByPowerBars", method=RequestMethod.POST)
+	public  @ResponseBody JSONObject getNearByPowerBars(HttpServletRequest req){
+		logger.trace("getNearByPowerBars method was invoked...");
+		Map<String,Object> likeCondition = new HashMap<String, Object>();
+		JSONObject Reobj=new JSONObject();
+		JSONArray  pbArray  = new JSONArray();
+
+		Reobj.put("result", "10001");
+		Reobj.put("powerbars", pbArray);
+		//先是处理输入
+		Hashtable<String, Object> jsonObj = (Hashtable<String, Object>)req.getAttribute("jsonArgs");
+		Double userLongitude = jsonObj.get("userLongitude")==null ? 0.0: Double.parseDouble(String.valueOf(jsonObj.get("userLongitude")));
+		Double userLatitude = jsonObj.get("userLatitude")==null ? 0.0: Double.parseDouble(String.valueOf(jsonObj.get("userLatitude")));
+		Long scope =   jsonObj.get("scope")==null ? 10L : Long.parseLong(String.valueOf(jsonObj.get("scope")));
+		Long start =   jsonObj.get("start")==null ? 0L : Long.parseLong(String.valueOf(jsonObj.get("start")));
+		if(start>9000) start =9000L;
+		Long limit =   jsonObj.get("limit")==null ? 20L : Long.parseLong(String.valueOf(jsonObj.get("limit")));
+		if(limit>10000L) limit=10000L;
+		Gps userWgs84 = CoordinatesUtil.gcj02_To_Gps84(userLatitude, userLongitude);
+		//开始构建SQL
+		/*
+		String querySQL = "SELECT id,ST_AsText(location) as location,extendinfo,desp,\"testArr\" from \"JOY_PowerBar\" where ST_DWithin(location,ST_GeographyFromText(\'POINT("
+				+userLongitude+" "
+				+userLatitude+")\'),"+scope+");";
+		*/
+		String queryCountSQL = "SELECT count(id) from \"JOY_PowerBar\" where (location <-> ST_GeomFromText(\'POINT("
+				+userLongitude+" "
+				+userLatitude+")\',4326)) < "+scope+"/111045::float limit 10000;";
+
+		System.out.println("queryCountSQL is "+queryCountSQL);
+		long qCount = jdbcTemplate.queryForLong(queryCountSQL);
+		if(qCount>0) {
+			Reobj.put("count",qCount);
+			if(limit>qCount) limit = qCount;
+			String querySQL = "SELECT id,ST_AsText(location) as location,extendinfo,desp,\"testArr\" from \"JOY_PowerBar\" ORDER BY location <-> ST_GeomFromText(\'POINT("
+					+userLongitude+" "
+					+userLatitude+")\',4326) limit "+limit +" offset "+start +";";
+			List items = jdbcTemplate.query(querySQL,
+					new JOYRowMapper(JOYPowerBar.class));
+			long sizeTop = (qCount - start)>0? (qCount - start) : 0;
+			if(sizeTop > items.size()) sizeTop = items.size();
+			for(int i=0;i<sizeTop;i++) {
+				JOYPowerBar pb = (JOYPowerBar)items.get(i);
+				pbArray.add(JOYPowerBar.toJSON(pb));
+			}
+			Reobj.put("result", "10000");
+		} else {
+			Reobj.put("count",0);
+			Reobj.put("result","10002");
+		}
+
+
+
+
+		return Reobj;
 	}
 
 	public static void main(String[] args) throws  Exception {
