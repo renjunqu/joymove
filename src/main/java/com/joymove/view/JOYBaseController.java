c6ca32e8492ee013b7f8cf474aad01e3dfd6d753
renjunqu <renjunqu@gmail.com>
2015-07-21 14:15:02 +0800

    完成了postgres嵌入的准备工作。

---
+++ src/main/java/com/joymove/view/JOYBaseController.java
@@ -0,0 +1,29 @@
+package com.joymove.view;
+
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.io.StringWriter;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by jessie on 2015/7/21.
+ */
+public class JOYBaseController  {
+
+    @ExceptionHandler(Exception.class)
+    public @ResponseBody JSONObject handleException(HttpServletRequest req,HttpServletResponse res,Exception ex){
+        JSONObject exJson = new JSONObject();
+        exJson.put("result","10001");
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        ex.printStackTrace(pw);
+        exJson.put("errMsg",sw.toString());
+        return exJson;
+    }
+
+
+}
--- src/main/java/com/joymove/view/JOYPowerBarController.java
+++ src/main/java/com/joymove/view/JOYPowerBarController.java
@@ -1,18 +1,22 @@
 package com.joymove.view;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.collections.iterators.EntrySetMapIterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,12 +27,34 @@ import com.joymove.service.JOYPowerBarService;
 
 
 @Controller("JOYPowerBarController")
-public class JOYPowerBarController {
+public class JOYPowerBarController extends  JOYBaseController {
 	@Resource(name = "JOYPowerBarService")
 	private  JOYPowerBarService joyPowerBarService;
 
+	@Resource(name="jdbcTemplate")
+	private JdbcTemplate jdbcTemplate;
+
+
+
+
 
 	final static Logger logger = LoggerFactory.getLogger(JOYPowerBarController.class);
+
+
+	@Transactional(propagation= Propagation.REQUIRED,rollbackFor = Exception.class)
+	public  List test(String sql) throws  Exception {
+		jdbcTemplate.execute("update \"JOY_PowerBar\" 12345 set id=10 where id=1;");
+		throw  new Exception("ahahah");
+
+	}
+
+	@RequestMapping(value="rent/ttt", method=RequestMethod.POST)
+	public  @ResponseBody JSONObject test(HttpServletRequest req) throws  Exception{
+		System.out.println("sdfsdf");
+		throw  new Exception("haha");
+	}
+
+
 
 
 
@@ -65,6 +91,26 @@ public class JOYPowerBarController {
 			 logger.error(e.getStackTrace().toString());
 		 }
 		 return Reobj;
+	}
+
+	public static void main(String[] args) throws  Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:**/applicationContext-mvc.xml");
+		JOYPowerBarController tempalte = (JOYPowerBarController)context.getBean("JOYPowerBarController");
+		System.out.println("hello, i am here\n");
+		List<Map<String, Object>> list = tempalte.test(" SELECT * from \"JOY_PowerBar\" where location = ST_GeomFromText('SRID=4326;POINT(3.2 2.4)') and \"extendinfo\" = '{\"q\":\"r\"}';");
+		System.out.println("get the result,hha");
+		for(int i=0;i<list.size();i++) {
+			System.out.println(i+" th line content:");
+		    Map<String,Object> map = list.get(i);
+			Set<Map.Entry<String, Object>> entrySet =  map.entrySet();
+			System.out.println("{");
+			Iterator iter = entrySet.iterator();
+			while(iter.hasNext()) {
+				Map.Entry<String, Object> entry = (Map.Entry<String, Object>)iter.next();
+				System.out.println("\t "+entry.getKey()+" : "+entry.getValue());
+			}
+			System.out.println("}");
+		}
 	}
 
 }
